cb5b32f91ce84bc2104c750aec0c2cb81638cacf
ArifulIslam99 <marifulislam0999@gmail.com>
2024-06-27 14:42:37 +0600

    Implement basic folder base routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Hello Bangladesh!</h2>
-    </main>
-  );
+    return <h2>Welcome to Home!</h2>
 }
