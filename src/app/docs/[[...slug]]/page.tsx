6992c42b660bb99151b6348878a76241d6716827
ArifulIslam99 <marifulislam0999@gmail.com>
2024-06-28 20:22:51 +0600

    Make sure optional catch all segment

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -11,7 +11,7 @@ export default function Docs({
         Docs from {params.slug[0]} of {params.slug[1]}{" "}
       </h2>
     );
-  } else if (params.slug.length == 1) {
+  } else if (params.slug?.length == 1) {
     return <h2> Docs from {params.slug[0]} </h2>;
   } else {
     return <h2>Docs Page</h2>;
