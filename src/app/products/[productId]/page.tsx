c3e4d7d2ad95cfcef9625749b4babf4a2bd32b85
ArifulIslam99 <marifulislam0999@gmail.com>
2024-06-29 21:51:54 +0600

    Generate dynamic metadata for each page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,14 @@
-export default function ProductId({
-  params,
-}: {
-  params: { productId: string };
-}) {
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `product ${params.productId}`,
+  };
+};
+export default function ProductId({ params }: Props) {
   return <h2>Product Details about {params.productId} </h2>;
 }
