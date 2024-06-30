3ac80177088f450e3e84c8533de7e53395238839
ArifulIslam99 <marifulislam0999@gmail.com>
2024-06-30 18:45:21 +0600

    Add navigation on dynamic route as well

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Product() {
-    return <h1>Product Details</h1>
+    return <h1><Link href={'/products/1'}>Product Details of page 1</Link></h1>
 }
