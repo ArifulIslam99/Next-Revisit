aa3e5c82e80bd2387b9c25a668f70bd6d62de949
ArifulIslam99 <marifulislam0999@gmail.com>
2024-07-02 22:48:13 +0600

    Use conditional routing

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,22 @@
+"use client";
+import { useState } from "react";
+
 export default function DashboardLayout({
   children,
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const [LoggedIn, setLoggedIn] = useState(true);
+
+  return LoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -17,8 +24,11 @@ export default function DashboardLayout({
           <div> {users} </div>
           <div> {revenue} </div>
         </div>
-        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div> <br />
+        <button onClick={()=> setLoggedIn(false)}>Sign Out</button>
       </div>
     </div>
+  ) : (
+    <div>{login}</div>
   );
 }
